84679f9d8f13ceb817e64190452f01127c157501
plokmij <samfan@live.com>
2019-02-16 11:40:09 +0530

    Add botttom navbar

--- lib/src/screens/newscreen.dart
+++ lib/src/screens/newscreen.dart
@@ -2,21 +2,32 @@ import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   Widget build(BuildContext context) {
+    print('Am I here?');
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('Home',style: TextStyle(color:Colors.black,fontWeight: FontWeight.bold),),
+      ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.black,
+        currentIndex: 1,
         items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home')
-          ),
+              icon: Icon(Icons.favorite), 
+              title: Text('Favorites')),
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             title: Text('Home'),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          )
+            icon: Icon(Icons.notifications),
+            title: Text('Alerts'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            title: Text('Profile'),
+          ),
         ],
       ),
     );
